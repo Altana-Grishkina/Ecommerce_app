51059048484e6403f25a57a3a31f206118a20809
Altn Grishkina <altanagrishkina@gmail.com>
2024-11-13 16:11:52 +0100

    Billing address

--- src/app/component/checkout/checkout.component.ts
+++ src/app/component/checkout/checkout.component.ts
@@ -6,13 +6,13 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.css'
 })
-export class CheckoutComponent implements OnInit{
+export class CheckoutComponent implements OnInit {
 
   checkoutFormGroup: FormGroup;
-  constructor(private formBuilder: FormBuilder){}
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
-      
+
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
         firstName: [''],
@@ -45,9 +45,19 @@ export class CheckoutComponent implements OnInit{
     });
   }
 
-  onSubmit(){
+  copyShippingAddressToBillingAddress(event) {
+
+    if (event.target.checked) {
+      this.checkoutFormGroup.controls['billingAddress']
+        .setValue(this.checkoutFormGroup.controls['shippingAddress'].value);
+    } else {
+      this.checkoutFormGroup.controls['billingAddress'].reset();
+    }
+  }
+
+  onSubmit() {
     console.log("Handling the submit button");
     console.log(this.checkoutFormGroup.get('customer').value);
-    console.log("The email address is "+this.checkoutFormGroup.get('customer').value.email);
+    console.log("The email address is " + this.checkoutFormGroup.get('customer').value.email);
   }
 }
