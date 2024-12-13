57e7e0b48826472f8fd4eb4949fcb44ec67c1bbb
Altn Grishkina <altanagrishkina@gmail.com>
2024-12-13 12:50:01 +0100

    Handle customer by email

--- src/main/java/com/luv2code/ecommerce/controller/CheckoutController.java
+++ src/main/java/com/luv2code/ecommerce/controller/CheckoutController.java
@@ -1,15 +1,9 @@
 package com.luv2code.ecommerce.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.luv2code.ecommerce.dto.Purchase;
 import com.luv2code.ecommerce.dto.PurchaseResponse;
 import com.luv2code.ecommerce.service.CheckoutService;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin("http://localhost:4200")
 @RestController
@@ -18,13 +12,12 @@ public class CheckoutController {
 
     private CheckoutService checkoutService;
 
-    @Autowired
-    private CheckoutController(CheckoutService checkoutService){
+    public CheckoutController(CheckoutService checkoutService) {
         this.checkoutService = checkoutService;
     }
 
     @PostMapping("/purchase")
-    public PurchaseResponse placeOrder(@RequestBody Purchase purchase){
+    public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
 
         PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
 
@@ -32,3 +25,12 @@ public class CheckoutController {
     }
 
 }
+
+
+
+
+
+
+
+
+
