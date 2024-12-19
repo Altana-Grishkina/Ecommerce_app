86e9785d53e3806efcea53814f4eaa33d27ea276
Altn Grishkina <altanagrishkina@gmail.com>
2024-12-19 12:57:13 +0100

    visualizza order by date created descendente

--- src/main/java/com/luv2code/ecommerce/dao/OrderRepository.java
+++ src/main/java/com/luv2code/ecommerce/dao/OrderRepository.java
@@ -10,6 +10,6 @@ import com.luv2code.ecommerce.entity.Order;
 @RepositoryRestResource
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
-    Page<Order> findByCustomerEmail(@Param("email") String email, Pageable pageable);
+    Page<Order> findByCustomerEmailOrderByDateCreatedDesc(@Param("email") String email, Pageable pageable);
 
 }
