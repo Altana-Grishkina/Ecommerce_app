7fa11436ed3821f6b84a518295664f12936881cc
Altn Grishkina <altanagrishkina@gmail.com>
2024-12-19 14:22:01 +0100

    prepopulate email field

--- src/app/component/checkout/checkout.component.ts
+++ src/app/component/checkout/checkout.component.ts
@@ -30,6 +30,8 @@ export class CheckoutComponent implements OnInit {
   shippingAddressStates: State[] = [];
   billingAddressStates: State[] = [];
 
+  storage: Storage = sessionStorage;
+
   constructor(private formBuilder: FormBuilder,
               private shopFormService: ShopFormService,
               private cartService: CartService,
@@ -41,6 +43,11 @@ export class CheckoutComponent implements OnInit {
 
     this.reviewCartDetails();
 
+    // read the user's email address from browser storage
+    const theEmail = JSON.parse(this.storage.getItem('userEmail')!);
+
+
+
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
         firstName: new FormControl('',
@@ -51,7 +58,7 @@ export class CheckoutComponent implements OnInit {
                             [Validators.required, 
                             Validators.minLength(2), 
                             ShopValidators.notOnlyWhitespace]),
-        email: new FormControl('', 
+        email: new FormControl(theEmail, 
                             [Validators.required, 
                             Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')])
       }),
