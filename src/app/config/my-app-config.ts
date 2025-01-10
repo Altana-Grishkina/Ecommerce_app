292367e4599bd1581e5e01d4d129c3b3df515571
Altn Grishkina <altanagrishkina@gmail.com>
2025-01-10 09:57:12 +0100

    Security - Order History - Secure Frontend - Create an Interceptor

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -1,9 +1,9 @@
 export default {
 
     oidc: {
-        clientId: 'o87yuhjg7gkjgggj',
-        issuer: 'https://dev-96978114.okta.com/oauth2/default',
+        clientId: '0oam6w1xukYsTxTb75d7',
+        issuer: 'https://dev-61750768.okta.com/oauth2/default',
         redirectUri: 'http://localhost:4200/login/callback',
-        scopes: ['openip', 'profile', 'email']
+        scopes: ['openid', 'profile', 'email']
     }
 }
