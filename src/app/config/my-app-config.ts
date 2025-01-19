d25b3b3546366440f2b863dd9fa9f5a3c1238ac3
Altn Grishkina <altanagrishkina@gmail.com>
2025-01-19 18:27:44 +0100

    add new environment

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -3,7 +3,7 @@ export default {
     oidc: {
         clientId: '0oam6w1xukYsTxTb75d7',
         issuer: 'https://dev-61750768.okta.com/oauth2/default',
-        redirectUri: 'http://localhost:4200/login/callback',
+        redirectUri: 'https://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email']
     }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -3,15 +3,16 @@ import { Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
 import { Product } from '../common/product';
 import { ProductCategory } from '../common/product-category';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  private baseUrl = 'http://localhost:8080/api/products';
+  private baseUrl = environment.shopApiUrl + '/products';
 
-  private categoryUrl = 'http://localhost:8080/api/product-category';
+  private categoryUrl = environment.shopApiUrl + '/product-category';
 
   constructor(private HttpClient: HttpClient) { }
 
@@ -30,7 +31,9 @@ export class ProductService {
     // @TODO: need to build URL based on category id, page and size
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`
      + `&page=${thePage}&size=${thePageSize}`;
-    
+
+
+    console.log(`Getting products from - ${searchUrl}`);
     return this.HttpClient.get<GetResponseProducts>(searchUrl);
   }
 
